37536880b3e95f7a8c10a107abc5e2bb7537d7a5
whitepom <jmstar870802@gmail.com>
2026-06-18 16:20:57 +0900

    마이너 Chord 추가

--- src/sheet/entities/sheet.entity.ts
+++ src/sheet/entities/sheet.entity.ts
@@ -2,7 +2,7 @@ import {IsIn, IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { emptyValidationMessage } from "src/common/validation-message/empty-validation.message";
 
 
-const keys = ['C','D','E','F','G','A','Ab','B','Bb',"Dm","Em","Gm","Bm"];
+const keys = ['C','D','E','F','G','A','Ab','B','Bb',"Cm","Dm","Em","Gm","Bm"];
 
 export class SheetModel {
 
